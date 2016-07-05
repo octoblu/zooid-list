e57f0567747d69b78b42587a02f3a256df7c3afe
Sqrt of Saturn <sqrt@octoblu.com>
2016-07-05 10:48:28 -0700

    v1.2.2 fix: linter issues

--- src/List/index.spec.js
+++ src/List/index.spec.js
@@ -1,9 +1,8 @@
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 import React from 'react'
-import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import { mount, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import List from './'
 
